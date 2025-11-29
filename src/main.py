b815714b5c01874932647c3c204a59192f293bac
mark-ads <mark.ads@list.ru>
2025-11-29 23:57:15 +0700

    Перенес роутеры в папку routers.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+
 from src.core.database import init_db
-from src.routers.posts import router as posts_router
-from src.routers.users import router as users_router
+from src.api.routers.posts import router as posts_router
+from src.api.routers.users import router as users_router
+from src.api.routers.admin import router as admin_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -11,5 +13,6 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-app.include_router(posts_router, prefix='/api/v1')
 app.include_router(users_router, prefix='/api/v1')
+app.include_router(posts_router, prefix='/api/v1')
+app.include_router(admin_router, prefix='/api/v1')
